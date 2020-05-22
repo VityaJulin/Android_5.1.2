aed7e3aef7bed1c272890b2e675e0f7dc3a80442
vityajulin <vkdvornokov@gmail.com>
2020-05-22 19:53:04 +0300

    add background image

---
+++ app/src/main/java/com/example/android_312/SettingsActivity.java
@@ -0,0 +1,101 @@
+package com.example.android_312;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.File;
+
+public class SettingsActivity extends AppCompatActivity {
+    private EditText fileName;
+    private Button okBtn;
+    private String name;
+    public ImageView imageBackground;
+    public ImageView imageBackgroundLand;
+    public static final int REQUEST_CODE_PERMISSION_WRITE_STORAGE = 100;
+    public static final int REQUEST_CODE_PERMISSION_READ_STORAGE = 10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        fileName = findViewById(R.id.file_edtx);
+        okBtn = findViewById(R.id.ok_btn);
+        imageBackground = findViewById(R.id.image_background);
+        imageBackgroundLand = findViewById(R.id.image_background_land);
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getPermissionStatus()) {
+                    LoadImg();
+                    Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    private void LoadImg() {
+        if (isExternalStorageWritable()) {
+            name = fileName.getText().toString();
+            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), name);
+            if (!file.exists() && !file.isFile()) {
+                Toast.makeText(this, "File not found", Toast.LENGTH_SHORT).show();
+            }
+            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+            imageBackground.setImageBitmap(bitmap);
+            imageBackgroundLand.setImageBitmap(bitmap);
+            fileName.setText(null);
+        } else {
+            Toast.makeText(this, "Storage unavailable", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_PERMISSION_READ_STORAGE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+        }
+    }
+
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean getPermissionStatus() {
+        int permissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSION_READ_STORAGE);
+            return false;
+        }
+    }
+}
